4acefe28f4af91baa7d65a9a1f358cce597dddc0
arslansherazi <arslanhaider.dev@hotmail.com>
2022-06-16 18:52:23 +0500

    add env varibales

--- configs/db.go
+++ configs/db.go
@@ -1,12 +1,16 @@
 package configs
 
 import (
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func GetDbInstance() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres password=postgres dbname=fnd_comp_db port=5432 sslmode=disable TimeZone=Asia/Karachi"
+
+	dsn := "host=" + os.Getenv("DB_HOST") + "user=" + os.Getenv("DB_USER") + "password=" + os.Getenv("DB_PASSWORD") +
+		"dbname=" + os.Getenv("DB_NAME") + "port=" + os.Getenv("PORT") + "sslmode=disable TimeZone=Asia/Karachi"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
--- settings/local.go
+++ settings/local.go
@@ -1,1 +1,11 @@
-package settings
+package main
+
+import "os"
+
+func main() {
+	os.Setenv("host", "localhost")
+	os.Setenv("user", "postgres")
+	os.Setenv("password", "postgres")
+	os.Setenv("dbname", "fnd_comp_db")
+	os.Setenv("posrt", "5432")
+}
