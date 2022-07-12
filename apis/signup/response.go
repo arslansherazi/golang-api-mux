e6b82161342cc709b70de14a734fb2bec17ce62c
arslansherazi <arslanhaider.dev@hotmail.com>
2022-07-13 03:29:59 +0500

    add data section in signup success response

--- apis/signup/response.go
+++ apis/signup/response.go
@@ -6,15 +6,21 @@ import (
 	"net/http"
 )
 
-type SuccessResponse struct {
+type BaseSuccessResponse struct {
 	IsSignedUp bool `json:"is_signed_up"`
+}
+
+type SuccessResponse struct {
+	Data BaseSuccessResponse `json:"data"`
 	common.BaseResponse
 }
 
 /* Response Functions */
 func generateSuccessResponse(requestUrl string, w http.ResponseWriter) {
 	successResponse := SuccessResponse{
-		IsSignedUp:   true,
+		Data: BaseSuccessResponse{
+			IsSignedUp: true,
+		},
 		BaseResponse: common.BaseResponse{StatusCode: 200, Success: true, Cmd: requestUrl},
 	}
 	json.NewEncoder(w).Encode(&successResponse)
